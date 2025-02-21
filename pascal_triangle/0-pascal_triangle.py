2247de2274ba94b28d35561bc73021c67cfec232
TAWE KELVIN <169045295+kelvintawe12@users.noreply.github.com>
2025-02-21 21:59:51 +0200

    remove unnecessary blank lines in pascal_triangle function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 Module to generate Pascal's triangle.
 """
 
+
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the nth row.
@@ -16,14 +17,10 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
     triangle = []
-    
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
-    
     return triangle
-
